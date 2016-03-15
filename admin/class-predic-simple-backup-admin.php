6b29cce5e6e5496748c635fb5923446d1368ce15
aca <aca@aca-shindiri-computer>
2016-03-15 17:07:49 +0100

    Changed flow, added list of backed up files in admin page, added new functions

--- admin/class-predic-simple-backup-admin.php
+++ admin/class-predic-simple-backup-admin.php
@@ -14,6 +14,7 @@ class Predic_Simple_Backup_Admin {
 		$this->plugin_name = $plugin_name;
 		$this->version = $version;
         $this->plugin_public_name = esc_html_x( 'Very Simple Backup', 'plugin public name and admin menu page name', 'predic-simple-backup' );
+        $this->plugin_admin_page = $this->plugin_name . '-page';
 
 	}
     
@@ -22,7 +23,7 @@ class Predic_Simple_Backup_Admin {
             $this->plugin_public_name . esc_html__( 'Options', 'predic-simple-backup' ), //  The text to be displayed in the title tags of the page when the menu is selected
             $this->plugin_public_name, // The text to be used for the menu
             'manage_options', // The capability required for this menu to be displayed to the user
-            $this->plugin_name . '-page', //  The slug name to refer to this menu by (should be unique for this menu)
+            $this->plugin_admin_page, //  The slug name to refer to this menu by (should be unique for this menu)
             array( $this, 'render_admin_page' ), // The function to be called to output the content for this page
             'dashicons-index-card', // Menu icon - https://developer.wordpress.org/resource/dashicons/
             79 // Position
@@ -39,13 +40,14 @@ class Predic_Simple_Backup_Admin {
          * 
          * napraviti da se dole listaju svi backup zip arhive sa velicinama fajla i datumom
          */
+        
         ?>
 
         <div class="psb-admin-page wrap">
             <h1><?php echo sprintf( esc_html__( '%s Settings', 'predic-simple-backup' ), $this->plugin_public_name ); ?></h1>
 
            <div class="psb-admin-page-content">
-               <p>SOME TEXT TO BE TRANSLATED</p>
+               <p><?php echo esc_html__( 'This plugin is for small sites that do not need fancy WP plugins for backup jobs. It zip all files from Your WP directory and add database dump into zip.', 'predic-simple-backup' ) ?></p>
 
            </div>
 
@@ -57,99 +59,187 @@ class Predic_Simple_Backup_Admin {
             </div>
         </div>
 
-        
-
         <?php
+        // List all backed files
+        $this->list_backup_files();
     }
     
-    public function make_site_backup() {
+    /*
+     * dir or url
+     * 
+     * return path without trailing slash
+     */
+    private function get_backup_directory( $parth_or_url = 'dir' ) {
         
-        /*
-        * PHP: Recursively Backup Files & Folders to ZIP-File
-        * plugin for very simple backup (database dump and whole wordpress zip)
-        */
+        $dir_name = 'predic-simple-backup';
         
         // Define files and folders
         $upload_dir = wp_upload_dir();
         $uploads_basedir = $upload_dir['basedir']; // Uploads basedir without slash
         $uploads_basedurl = $upload_dir['baseurl']; // Uploads basedir without slash
-        $directory = ABSPATH; // With end trailing slash
+        
+        // Make directory in uploads folder to store backups, if don't exist
+        $backup_files_dir = $uploads_basedir . '/' . $dir_name;
+        
+        if ( ! file_exists(realpath($backup_files_dir) ) ) {
+            if ( ! mkdir($backup_files_dir, 0755) ) {
+                wp_die( esc_html__( 'Can not create parent directory to store backup files' ) );
+            }
+        }
+        
+        if ( $parth_or_url === 'dir' ) {
+            return $backup_files_dir;
+        } else {
+            return $uploads_basedurl . '/' . $dir_name;
+        }
+        
+        
+    }
+    
+    public function make_site_backup() {
+
+        /*
+        * PHP: Recursively Backup Files & Folders to ZIP-File
+        */
+        
+        // Define files and folders
+        $backup_files_dir = $this->get_backup_directory();
+        if ( ! $backup_files_dir ) {
+            wp_die( esc_html__( 'Directory to store backup files does not exist' ) );
+        }
 
         // Folder to backup and zip name and path
         $zip_name = strtolower( sanitize_file_name( get_bloginfo( 'name' ) ) . date("Y-m-d-h-i-sa") ) .".zip";
-        $destination = $uploads_basedir . '/' . $zip_name; // Destination dir and filename
-        $destination_download_link = $uploads_basedurl . '/' . $zip_name; // Destination url and filename
-        $source = ABSPATH; // the folder which you archivate
-
-
+        $destination = $backup_files_dir . '/' . $zip_name; // Destination dir and filename
+        $directory = ABSPATH; // The folder which you archivate
+        
+        
         // Make sure the script can handle large folders/files
         $this->bypass_server_limit();
 
         // Start the backup!
-        if ( $this->zipData($source, $destination) ) {
-            echo 'Finished.';
-            echo '<a href="' . $destination_download_link . '" target="_blank">Click here</a> to download your backup zip archive.';
+        if ( $this->zipData($directory, $destination) ) {
+
+            // redirect to plugin page
+            $admin_page_url = admin_url( 'admin.php?page=' . $this->plugin_admin_page );
+            $this->redirect( $admin_page_url );
+
         } else {
-            echo 'Operation failed';
+            
+            wp_die( esc_html__( 'Something went wront while archiving the site.' ) );
+            
         }
         
     }
     
     // Here the magic happens :)
-    private function zipData($source, $destination) {
+    private function zipData($directory, $destination) {
 
         $upload_dir = wp_upload_dir();
         // Uploads basedir without slash
         $uploads_basedir = $upload_dir['basedir'];
+        // Set database name
+        $database_add_to_root = sanitize_file_name( 'database-' . DB_NAME . date("Y-m-d") ) . '.sql';
+        $database_filename = $uploads_basedir . '/' . $database_add_to_root;
 
         if (extension_loaded('zip')) {
-                if (file_exists($source)) {
-                    $zip = new ZipArchive();
-                    if ($zip->open($destination, ZIPARCHIVE::CREATE)) {
-                            $source = realpath($source);
-                            if (is_dir($source)) {
-                                    $iterator = new RecursiveDirectoryIterator($source);
-                                    // skip dot files while iterating 
-                                    $iterator->setFlags(RecursiveDirectoryIterator::SKIP_DOTS);
-                                    $files = new RecursiveIteratorIterator($iterator, RecursiveIteratorIterator::SELF_FIRST);
-                                    foreach ($files as $file) {
-                                            $file = realpath($file);
-                                            if (is_dir($file)) {
-                                                    $zip->addEmptyDir(str_replace($source . '/', '', $file . '/'));
-                                            } else if (is_file($file)) {
-                                                    $zip->addFromString(str_replace($source . '/', '', $file), file_get_contents($file));
-                                            }
-                                    }
-                            } else if (is_file($source)) {
-                                    $zip->addFromString(basename($source), file_get_contents($source));
+            
+            if (file_exists($directory)) {
+                
+                $zip = new ZipArchive();
+
+                if ($zip->open($destination, ZIPARCHIVE::CREATE)) {
+
+                        $directory = realpath($directory);
+                        
+                        if (is_dir($directory)) {
+                            
+                            $iterator = new RecursiveDirectoryIterator($directory);
+                            // skip dot files while iterating 
+                            $iterator->setFlags(RecursiveDirectoryIterator::SKIP_DOTS);
+                            $files = new RecursiveIteratorIterator($iterator, RecursiveIteratorIterator::SELF_FIRST);
+                            
+                            foreach ($files as $file) {
+                                $file = realpath($file);
+                                if (is_dir($file)) {
+                                        $zip->addEmptyDir(str_replace($directory . '/', '', $file . '/'));
+                                } else if (is_file($file)) {
+                                        $zip->addFromString(str_replace($directory . '/', '', $file), file_get_contents($file));
+                                }
                             }
+                            
+                        } else if (is_file($directory)) {
+                                $zip->addFromString(basename($directory), file_get_contents($directory));
+                        }
 
-                            // Try to export database and add it to the zip
-                            try {
-                                $database_add_to_root = sanitize_file_name( 'database-' . DB_NAME . date("Y-m-d") ) . '.sql';
-                                $database_filename = $uploads_basedir . '/' . $database_add_to_root;
-                                exec('mysqldump --add-drop-table --user=' . DB_USER . ' --password=' . DB_PASSWORD . ' --host=' . DB_HOST . ' ' . DB_NAME . ' > ' . $database_filename);
-                                $zip->addFromString(str_replace($source . '/', '', $database_add_to_root), file_get_contents($database_filename));
-                                unlink($database_filename);
+                        // Try to export database and add it to the zip
+                        try {
+                            exec('mysqldump --add-drop-table --user=' . DB_USER . ' --password=' . DB_PASSWORD . ' --host=' . DB_HOST . ' ' . DB_NAME . ' > ' . $database_filename);
+                            $zip->addFromString(str_replace($directory . '/', '', $database_add_to_root), file_get_contents($database_filename));
+                            unlink($database_filename);
+                            
+                           
 
-                                echo "<span style='color:green;'>database export file is added to zip file.<br /></span> " ;
+                           // echo "<span style='color:green;'>database export file is added to zip file.<br /></span> " ;
 
-                            } catch(Exception $e) {
-                              echo 'Error message (there was something wrong while exporting database): ' .$e->getMessage();
-                            }
+                        } catch(Exception $e) {
+                            
+                            wp_die( esc_html__( 'Error message (there was something wrong while exporting database): ', 'predic-simple-backup' ) .$e->getMessage() );
 
-                    }
-                    return $zip->close();
+                        }
+
                 }
+
+                if ( $zip->close() ) {
+                    // Set mode for newly created archive file
+                    chmod($destination, 0644);
+                    return true;
+                } else {
+                    return false;
+                }
+                
+            }
+            
         }
+        
         return false;
+        
+    }
+    
+    private function list_backup_files() {
+        
+        $backup_dir = $this->get_backup_directory();
+        $backup_dir_url = $this->get_backup_directory( 'url' );
+        
+        $files = scandir( $backup_dir );
+        
+        echo '<h3>' . esc_html__( 'List of backups', 'predic-simple-backup' ) . '</h3>';
+        
+        echo '<ul>';
+        
+        foreach ( $files as $file ) {
+            
+            if ( $file === '.' || $file === '..' ) {
+                continue;
+            }
+            
+            echo '<li><a href="' . esc_url( $backup_dir_url . '/' . $file ) . '">' . $file . '</a></li>';
+        }
+        
+        echo '</ul>';
+        
+    }
+    
+    private function redirect( $destination ) {
+        wp_redirect( $destination ); 
+        die();
     }
     
     /**
     * Bypass limit server if possible
     * @since 1.0.0
     */
-   public static function bypass_server_limit() {
+   private function bypass_server_limit() {
        @ini_set('memory_limit','1024M');
        @ini_set('max_execution_time','0');
    }
