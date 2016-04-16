3177b8ee55f7222075bbe4b30a1e8219623a0fbe
aca <aca@aca-toshiba>
2016-04-16 18:14:46 +0200

    Edit comment

--- includes/class-predic-simple-backup.php
+++ includes/class-predic-simple-backup.php
@@ -1,9 +1,7 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/*
+ * Main plugin class
  */
 
 class Predic_Simple_Backup {
